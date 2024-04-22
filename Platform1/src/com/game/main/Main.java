b3aa651a37eb7ae764487f958b45cf3c4fe17769
josefortyfive <jose.fortyfive@gmail.com>
2024-04-22 21:02:42 +0800

    Add to 2DGame repository

---
+++ Platform1/src/com/game/main/Main.java
@@ -0,0 +1,18 @@
+package com.game.main;
+
+import com.game.gamepanel.GamePanel;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String args[]){
+        JFrame window = new JFrame("First Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.setContentPane(new GamePanel());
+
+        window.pack();
+        window.setVisible(true);
+    }
+}
