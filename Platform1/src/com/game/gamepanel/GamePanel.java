ba0f6a64d758f50d03ee862a218d3326ba3d9558
josefortyfive <jose.fortyfive@gmail.com>
2024-04-22 21:35:11 +0800

    Added key input

--- Platform1/src/com/game/gamepanel/GamePanel.java
+++ Platform1/src/com/game/gamepanel/GamePanel.java
@@ -1,9 +1,13 @@
 package com.game.gamepanel;
 
+import com.game.player.Player;
+
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.*;
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable, KeyListener {
 
     public static int WIDTH = 400;
     public static int HEIGHT = 400;
@@ -14,6 +18,9 @@ public class GamePanel extends JPanel implements Runnable{
     private Graphics2D g;
     private int FPS = 30;
     private double averageFPS;
+
+
+    private Player player;
 
     public GamePanel(){
         super();
@@ -30,6 +37,8 @@ public class GamePanel extends JPanel implements Runnable{
             thread = new Thread(this);
             thread.start();
         }
+
+        addKeyListener(this);
     }
 
     @Override
@@ -38,6 +47,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
         g = (Graphics2D) image.getGraphics();
+        player = new Player();
 
         long startTime;
         long URDTimeMillis;
@@ -79,7 +89,7 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     private void gameUpdate(){
-
+        player.update();
     }
 
     private void gameRender(){
@@ -87,11 +97,60 @@ public class GamePanel extends JPanel implements Runnable{
         g.fillRect(0, 0, WIDTH, HEIGHT);
         g.setColor(Color.BLACK);
         g.drawString("FPS: " + averageFPS, 10, 10);
+        player.draw(g);
     }
 
     private void gameDraw(){
         Graphics g2 = this.getGraphics();
         g2.drawImage(image, 0, 0 , null);
         g2.dispose();
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent key) {
+        int keyCode = key.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_LEFT){
+            player.setLeft(true);
+        }
+
+        if(keyCode == KeyEvent.VK_RIGHT){
+            player.setRight(true);
+        }
+
+        if(keyCode == KeyEvent.VK_UP){
+            player.setUp(true);
+        }
+
+        if(keyCode == KeyEvent.VK_DOWN){
+            player.setDown(true);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent key) {
+        int keyCode = key.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_LEFT){
+            player.setLeft(false);
+        }
+
+        if(keyCode == KeyEvent.VK_RIGHT){
+            player.setRight(false);
+        }
+
+        if(keyCode == KeyEvent.VK_UP){
+            player.setUp(false);
+        }
+
+        if(keyCode == KeyEvent.VK_DOWN){
+            player.setDown(false);
+        }
     }
 }
